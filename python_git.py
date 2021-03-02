974d411b49cc3f45c93b18d287c97b710266ea41
NIDA GHANI <nidaghani00@gmail.com>
2021-03-02 21:40:11 +0530

    first commit

---
+++ python_git.py
@@ -0,0 +1,5 @@
+#rabdom program
+a=int(input("how are you"))
+b=int(input("how am I?"))
+c=a+b
+print("the output is",c)
---
+++ python_git_2.py
@@ -0,0 +1,6 @@
+#random python code
+a=int(input("please enter the number of times the word should be repeated"))
+b=input("please enter the word")
+for i in range(a):
+    print(b)
+
